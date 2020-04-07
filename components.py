39d9b6b426175baca6918260e798da1e3518fa58
lnxpy <lnxpylnxpy@gmail.com>
2020-04-07 22:07:52 +0430

    bas pushed & getter class created

---
+++ components.py
@@ -0,0 +1,40 @@
+import requests
+
+config = {
+    'main_address': 'http://codehub.pythonanywhere.com/api/',
+    'api_version': 'v1'
+}
+
+
+class GetSnippet:
+
+    def __init__(self, SID):
+        self.SID = SID
+
+    def get(self):
+        url = '%s%s' % (config['main_address'] +
+                        config['api_version'] + '/snippet/', self.SID)
+        r = requests.get(url)
+        return r.json()
+
+
+class PushSnippet:
+
+    def __init__(self, title, details, script, language):
+        self.title = title
+        self.details = details
+        self.script = script
+        self.language = language
+
+    def push(self):
+        url = '%s' % (config['main_address'] +
+                      config['api_version'] + '/snippet/')
+        structure = {
+            'title': self.title,
+            'details': self.details,
+            'script': self.script,
+            'language': self.language,
+        }
+        r = requests.post(url, json=structure)
+
+        return r.json()
