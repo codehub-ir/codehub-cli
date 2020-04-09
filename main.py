159010ad268487a3074ab546343552d732b13486
lnxpy <lnxpylnxpy@gmail.com>
2020-04-09 15:28:42 +0430

    main file restored well

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 from components import GetSnippet, PushSnippet, Language
 
 # Here you code..
-
-lan = Language()
-snippet = PushSnippet('t', 'd', 's', lan.swift)
-res = snippet.push()
-
-print(res)
